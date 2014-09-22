39286fdf8933ea51988e3a28bb17604646ba6656
Disavowed13 <iceviper357@yahoo.com>
2014-09-22 00:03:28 -0700

    Create NeutralExplosives.hpp

---
+++ NeutralExplosives.hpp
@@ -0,0 +1,37 @@
+class Category_529 {
+	class HandGrenade_west {
+		type = "trade_items";
+		buy[] ={4000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class 1Rnd_HE_M203 {
+		type = "trade_items";
+		buy[] ={4000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class HandGrenade_east {
+		type = "trade_items";
+		buy[] ={4000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class Stinger {
+		type = "trade_items";
+		buy[] ={20000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class MAAWS_HEAT {
+		type = "trade_items";
+		buy[] ={20000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class MAAWS_HEDP {
+		type = "trade_items";
+		buy[] ={20000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+	class Vil_PZF3_S_HEAT {
+		type = "trade_items";
+		buy[] ={20000,"Coins"};
+		sell[] ={2000,"Coins"};
+	};
+};
