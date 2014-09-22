4d8b6b29c4dcb617c874f1c1ed8da45acc570392
Disavowed13 <iceviper357@yahoo.com>
2014-09-21 23:05:05 -0700

    Create Extras.hpp

---
+++ Extras.hpp
@@ -0,0 +1,183 @@
+//Vending Machine
+class Category_2010 {
+	class ItemWaterbottleUnfilled {
+		type = "trade_items";
+		buy[] ={4000,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemSodaCoke {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemSodaPepsi {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemSodaOrangeSherbet {
+		type = "trade_items";
+		buy[] ={2500,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemSodaR4z0r {
+		type = "trade_items";
+		buy[] ={2500,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class FoodMRE {
+		type = "trade_items";
+		buy[] ={100,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class FoodNutmix {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class FoodPistachio {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class FoodrabbitCooked {
+		type = "trade_items";
+		buy[] ={300,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+};
+class Category_2011 {
+	class AKS_GOLD {
+		type = "trade_weapons";
+		buy[] ={2700,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class revolver_gold_EP1 {
+		type = "trade_weapons";
+		buy[] ={1500,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class MakarovSD {
+		type = "trade_weapons";
+		buy[] ={400,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class 6Rnd_45ACP {
+		type = "trade_items";
+		buy[] ={10,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class 8Rnd_9x18_MakarovSD {
+		type = "trade_items";
+		buy[] ={10,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class 30Rnd_762x39_AK47 {
+		type = "trade_items";
+		buy[] ={50,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+};
+class Category_2012 {
+	class DZ_British_ACU {
+		type = "trade_backpacks";
+		buy[] ={2000,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class US_Assault_Pack_EP1 {
+		type = "trade_backpacks";
+		buy[] ={1500,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class DZ_GunBag_EP1 {
+		type = "trade_backpacks";
+		buy[] ={3000,"Coins"};
+		sell[] ={0,"Coins"};
+    };
+};
+class Category_2013 {
+	class ItemBloodbag {
+		type = "trade_items";
+		buy[] ={100,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemPainkiller {
+		type = "trade_items";
+		buy[] ={50,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemBandage {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemMorphine {
+		type = "trade_items";
+		buy[] ={100,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+};
+class Category_2014 {
+	class ItemEtool {
+		type = "trade_weapons";
+		buy[] ={1500,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemToolbox {
+		type = "trade_weapons";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemCrowbar {
+		type = "trade_weapons";
+		buy[] ={30,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemFishingPole {
+		type = "trade_weapons";
+		buy[] ={300,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+	class ItemRadio {
+		type = "trade_weapons";
+		buy[] ={50,"Coins"};
+		sell[] ={0,"Coins"};
+	};
+};
+class Category_2015 {
+	class PartOreGold {
+		type = "trade_items";
+		buy[] ={999999,"Coins"};
+		sell[] ={1000,"Coins"};
+	};
+	class PartOreSilver {
+		type = "trade_items";
+		buy[] ={999999,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+};
+//drug dealer
+class Category_2000 {
+	class ItemKiloHemp {
+		type = "trade_items";
+		buy[] ={30000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+};
+class Category_2016 {
+	class M1130_CV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={500000,"Coins"};
+		sell[] ={150000,"Coins"};
+	};
+	class M1126_ICV_M2_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={500000,"Coins"};
+		sell[] ={150000,"Coins"};
+	};
+	class M1126_ICV_mk19_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={500000,"Coins"};
+		sell[] ={150000,"Coins"};
+	};
+};
