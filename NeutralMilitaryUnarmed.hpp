6aad90c14c32efeb38387263daddef240b54d07c
Disavowed13 <iceviper357@yahoo.com>
2014-09-22 14:14:40 -0700

    Create NeutralMilitaryUnarmed.hpp

---
+++ NeutralMilitaryUnarmed.hpp
@@ -0,0 +1,124 @@
+class Category_658 {
+	class LandRover_CZ_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class LandRover_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class HMMWV_Terminal_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_M1035_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_Ambulance {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_Ambulance_CZ_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_DZ {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class M1133_MEV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class M113Ambul_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class M113Ambul_UN_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class GAZ_Vodnik_MedEvac {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+};
+class Category_598 {
+	class LandRover_CZ_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class LandRover_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class HMMWV_Terminal_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_M1035_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_Ambulance {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_Ambulance_CZ_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class HMMWV_DZ {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class M1133_MEV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class GAZ_Vodnik_MedEvac {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class M113Ambul_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class M113Ambul_UN_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+};
