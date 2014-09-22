d0539caa4f5bfa82eab7db45cd84562c29c45266
Disavowed13 <iceviper357@yahoo.com>
2014-09-21 22:56:19 -0700

    Create BanditTrucksArmed.hpp

---
+++ BanditTrucksArmed.hpp
@@ -0,0 +1,97 @@
+class Category_534 {
+	class ArmoredSUV_PMC_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class Pickup_PK_TK_GUE_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_DSHKM_Gue_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Pickup_PK_GUE_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class Pickup_PK_INS_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class LandRover_MG_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class LandRover_MG_TK_INS_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class LandRover_SPG9_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class LandRover_SPG9_TK_INS_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class Offroad_DSHKM_TERROR {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_DSHKM_Gue {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_DSHKM_INS {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class EOffroad_DSHKM_TERROR {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_DSHKM_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_SPG9_Gue {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Offroad_SPG9_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+		class Pickup_PK_GUE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Pickup_PK_INS {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Pickup_PK_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+};
