e4036f832fab70e52519fe12343aa8f9419d7e09
Disavowed13 <iceviper357@yahoo.com>
2014-09-22 00:02:22 -0700

    Create NeutralCargoTrucks.hpp

---
+++ NeutralCargoTrucks.hpp
@@ -0,0 +1,245 @@
+class Category_653 {
+	class Kamaz {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class KamazOpen_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class KamazReammo {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class KamazRepair {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class MTVR {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class MtvrReammo {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class MtvrRepair {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class MTVR_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class MtvrReammo_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class MtvrRepair_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class UralCivil_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class UralCivil2_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={5000,"Coins"};
+	};
+	class Ural_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class Ural_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class Ural_UN_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class UralReammo_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class UralReammo_INS {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class UralReammo_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class V3S_Open_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Open_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Civ {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_TK_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Reammo_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class V3S_Repair_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	
+};
+class Category_586 {
+	class Kamaz {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class KamazOpen_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class KamazReammo {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class KamazRepair {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class MTVR {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class MtvrReammo {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class MtvrRepair {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class MTVR_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class MtvrReammo_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class MtvrRepair_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+	class UralCivil_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class UralCivil2_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={5000,"Coins"};
+	};
+	class Ural_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class Ural_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class Ural_UN_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class UralReammo_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class UralReammo_INS {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class UralReammo_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class V3S_Open_TK_CIV_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Open_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Civ {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_TK_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class V3S_Reammo_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={40000,"Coins"};
+		sell[] ={20000,"Coins"};
+	};
+	class V3S_Repair_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={30000,"Coins"};
+		sell[] ={15000,"Coins"};
+	};
+};
