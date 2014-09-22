e0891e02298d1fbf669345aa08f7a622d7a63769
Disavowed13 <iceviper357@yahoo.com>
2014-09-21 23:44:06 -0700

    Create HeroWeapons.hpp

---
+++ HeroWeapons.hpp
@@ -0,0 +1,76 @@
+class Category_477 {
+	class G36_C_SD_camo {
+		type = "trade_weapons";
+		buy[] ={2500,"Coins"};
+		sell[] ={1250,"Coins"};
+	};
+	class BAF_L85A2_RIS_Holo {
+		type = "trade_weapons";
+		buy[] ={2500,"Coins"};
+		sell[] ={1250,"Coins"};
+	};
+	class BAF_L85A2_RIS_SUSAT {
+		type = "trade_weapons";
+		buy[] ={2500,"Coins"};
+		sell[] ={1250,"Coins"};
+	};
+	class BAF_L86A2_ACOG {
+		type = "trade_weapons";
+		buy[] ={2500,"Coins"};
+		sell[] ={1250,"Coins"};
+	};
+	class M4A1_AIM_SD_camo {
+		type = "trade_weapons";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class AA12_PMC {
+		type = "trade_weapons";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class FN_FAL_ANPVS4 {
+		type = "trade_weapons";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class SCAR_H_LNG_Sniper_SD {
+		type = "trade_weapons";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class m107_DZ {
+		type = "trade_weapons";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class BAF_AS50_scoped {
+		type = "trade_weapons";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class BAF_LRR_scoped_W {
+		type = "trade_weapons";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class BAF_LRR_scoped {
+		type = "trade_weapons";
+		buy[] ={75000,"Coins"};
+		sell[] ={37500,"Coins"};
+	};
+	class FN_FAL {
+		type = "trade_weapons";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class Mk_48_DZ {
+		type = "trade_weapons";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class M240_DZ {
+		type = "trade_weapons";
+		buy[] ={20000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
