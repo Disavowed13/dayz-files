ca389a6f429a13b3c9e7a764912de5512533b2b7
Disavowed13 <iceviper357@yahoo.com>
2014-09-21 22:41:51 -0700

    Create BanditMilitaryArmed.hpp

---
+++ BanditMilitaryArmed.hpp
@@ -0,0 +1,157 @@
+class Category_569 {
+	class BRDM2_HQ_Gue {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class BRDM2_HQ_TK_GUE_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class HMMWV_M998A2_SOV_DES_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class HMMWV_M998A2_SOV_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_M1151_M2_CZ_DES_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class HMMWV_M1151_M2_CZ_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_M2_USArmy {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_M2 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_Mk19_USArmy {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_MK19 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_MK19_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class HMMWV_Armored {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class LandRover_Special_CZ_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class LandRover_MG_TK_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class UAZ_MG_TK_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class GAZ_Vodnik_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class BAF_Jackal2_GMG_D {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class BAF_Jackal2_GMG_W {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class BAF_Jackal2_L2A1_D {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class BAF_Jackal2_L2A1_W {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class BTR90_HQ {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class GAZ_Vodnik {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class UAZ_AGS30_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_AGS30_INS {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_AGS30_RU {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_AGS30_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_MG_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_MG_INS {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_SPG9_INS {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class UAZ_MG_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class BTR40_MG_TK_INS_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+};
